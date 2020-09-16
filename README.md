dc5bf12eec3de7018d2f935e712c8664414085e6
Amey Satwe <amey10051999@gmail.com>
2020-09-16 14:46:15 +0530

    added tp routes

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Factory-Backend-Project
+
 This repo will have our django/node based api server
+
+- [ ] Machine Name
+- [ ] All vars from tool.py
+- [ ]
--- app.js
+++ app.js
@@ -2,14 +2,31 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-const { mongoose } = require("./db/mongoose");
+//const { mongoose } = require("./db/mongoose");
 const port = process.env.PORT;
+
 app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send({
     name: "Amey",
     email: "asatwe@gmail.com",
   });
+});
+
+app.post("/operation", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
+
+app.post("/tool", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
+
+app.post("/wear", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
 });
 
 app.listen(port, () => {
