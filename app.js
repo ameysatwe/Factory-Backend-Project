6914a58b32b5077f77479f761b83ff0a94fc3c64
Amey Satwe <amey10051999@gmail.com>
2020-09-17 16:40:46 +0530

    added some random data

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cors = require("cors");
 //const { mongoose } = require("./db/mongoose");
 const port = process.env.PORT;
-
+app.use(morgan("dev"));
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -22,6 +24,14 @@ app.post("/operation", (req, res) => {
 app.post("/tool", (req, res) => {
   console.log(req.body);
   res.send(req.body);
+});
+app.get("/status/:id", (req, res) => {
+  let statuses = [];
+  for (let i = 0; i < 10; i++) {
+    if (i % 2 == 0) statuses.push("Off");
+    statuses.push("ON");
+  }
+  res.send({ no: req.params.id, code: statuses[req.params.id] });
 });
 
 app.post("/wear", (req, res) => {
